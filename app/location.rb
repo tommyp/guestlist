9f11c22db8506965c2844756cde8a7739d27f358
Tommy Palmer <hi@tommyp.org>
2020-03-29 23:44:58 +0100

    add Location#kilometres_to_dublin_office
    This method calculates the distance between the lat and long on `self`
    and the one from the Dublin office. We do this by using the
    [first formula listed on
    Wikipedia](https://en.wikipedia.org/wiki/Great-circle_distance#Formulae).
    
    This PR also adds 2 tests for calculating the distance from London to
    Dublin and Belfast to Dublin.

--- app/location.rb
+++ app/location.rb
@@ -1,8 +1,29 @@
 class Location
+  DUBLIN_OFFICE_LATITUDE = 53.339428
+  DUBLIN_OFFICE_LONGITUDE = -6.257664
+
+  RADIANS_PER_DEGREE = Math::PI/180
+  EARTH_RADIUS_IN_KM = 6371
+  RADIUS_IN_METRES = EARTH_RADIUS_IN_KM * 1000
+
   attr_reader :lat, :lng
 
   def initialize(lat:, lng:)
     @lat = lat
     @lng = lng
+  end
+
+  def kilometres_to_dublin_office
+    (metres_to_dublin_office / 1000).round
+  end
+
+  private
+
+  def metres_to_dublin_office
+    cos_delta = Math.cos(lng * RADIANS_PER_DEGREE - DUBLIN_OFFICE_LONGITUDE * RADIANS_PER_DEGREE)
+    sin_formula = Math.sin(lat * RADIANS_PER_DEGREE) * Math.sin(DUBLIN_OFFICE_LATITUDE * RADIANS_PER_DEGREE)
+    cos_formula = Math.cos(lat * RADIANS_PER_DEGREE) * Math.cos(DUBLIN_OFFICE_LATITUDE * RADIANS_PER_DEGREE) * cos_delta
+
+    Math.acos(sin_formula + cos_formula) * RADIUS_IN_METRES
   end
 end
--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -3,17 +3,40 @@ require_relative "../app/location"
 
 describe Location do
   subject { described_class.new(lat: lat, lng: lng) }
-  let(:lat) { nil }
-  let(:lng) { nil }
 
-  it { is_expected.to respond_to(:lat) }
-  it { is_expected.to respond_to(:lng) }
+  describe "#new" do
+    let(:lat) { nil }
+    let(:lng) { nil }
 
-  context "with a lat and a lng" do
-    let(:lat) { 51.5285582 }
-    let(:lng) { -0.2416808 }
+    it { is_expected.to respond_to(:lat) }
+    it { is_expected.to respond_to(:lng) }
 
-    its(:lat)     { is_expected.to eq(51.5285582) }
-    its(:lng)     { is_expected.to eq(-0.2416808) }
+    context "with a lat and a lng" do
+      let(:lat) { 51.5285582 }
+      let(:lng) { -0.2416808 }
+
+      its(:lat) { is_expected.to eq(51.5285582) }
+      its(:lng) { is_expected.to eq(-0.2416808) }
+    end
+  end
+
+  describe "#kilometres_to_dublin_office" do
+    context 'from London to Dublin' do
+      let(:lat) { 51.5055481 }
+      let(:lng) { -0.1329171 }
+
+      it "calculates the distance" do
+        expect(subject.kilometres_to_dublin_office).to eq(462)
+      end
+    end
+
+    context 'from Belfast to Dublin' do
+      let(:lat) { 54.5965421 }
+      let(:lng) { -5.9523907 }
+
+      it "calculates the distance" do
+        expect(subject.kilometres_to_dublin_office).to eq(141)
+      end
+    end
   end
 end
