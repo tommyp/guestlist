bc3ad855c3162a46867e29f5b3c1f156fdea2c90
Tommy Palmer <hi@tommyp.org>
2020-03-31 00:43:03 +0100

    add a main.rb for running the program
    The `main.rb` will be the entry point for running the program and pass
    the arguments to `Guestlist.rb`.

---
+++ main.rb
@@ -0,0 +1,14 @@
+require_relative './app/guestlist'
+
+path = ARGV[0]&.to_s
+distance = ARGV[1]&.sub('km', '')&.to_i
+
+raise ArgumentError, "You must supply a file path to a list of customers as the first argument" if path.nil? || path == ""
+raise ArgumentError, "You must supply a kilometre distance greater than 0" if distance.nil? || distance <= 0
+
+puts "Customers to invite within #{distance}km:"
+puts "user_id | name"
+puts Guestlist.new(
+  customer_data_file_path: path,
+  distance: distance,
+).print
